c20bc1120e6cc7d3c2985d684d77bca052d4e2b3
hamzahabeeb <71086325+hamzahabeeb@users.noreply.github.com>
2023-10-27 11:09:39 +0530

    Update chat_room.py
    realtime messages

--- chat/frappe_chat/doctype/chat_room/chat_room.py
+++ chat/frappe_chat/doctype/chat_room/chat_room.py
@@ -1,11 +1,14 @@
 # Copyright (c) 2021, codescientist703 and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class ChatRoom(Document):
 	def get_members(self):
 		if self.members:
-			return [x.strip() for x in self.members.split(",")]
+			members = [x.strip() for x in self.members.split(",")]
+			for i in self.users:
+				members.append(i.user)
+			return frappe.utils.unique(members)
 		return []
